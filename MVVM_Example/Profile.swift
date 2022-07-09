1eca21890e2d84937fc3385d73fcaa953854f9fa
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-09 23:02:51 +0300

    basic MVC prep up

---
+++ MVVM_Example/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM_Example
+//
+//  Created by test on 09.07.2022.
+//
+
+import Foundation
+
+struct Profile{
+    var name: String
+    var secondName: String
+    var age: Int
+}
